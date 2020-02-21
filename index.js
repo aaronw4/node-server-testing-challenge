7fd662d671528861387d534910cc399f24040ab9
Aaron Williams <aaronrw4@yahoo.com>
2020-02-20 20:08:02 -0800

    Fixed index.js / added env.port

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const server = require('./server');
 
-server.listen(5000, () => {
-    console.log(`Listening on http://localhost:5000`)
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`)
 })
