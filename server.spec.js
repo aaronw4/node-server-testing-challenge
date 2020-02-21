d59cd0068d47692f5f9e1efae23ced00d5528f89
Aaron Williams <aaronrw4@yahoo.com>
2020-02-20 22:32:39 -0800

    removed unneeded code from server.js

--- server.spec.js
+++ server.spec.js
@@ -1,8 +1,6 @@
-const server = require('./server');
-const request = require('supertest');
 
 describe('server.js', () => {
     test('Should be the testing environment.', () => {
         expect(process.env.DB_ENV).toBe('testing')
     })
-})
+});
